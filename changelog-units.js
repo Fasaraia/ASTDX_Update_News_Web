ced89f6f1a005ac38003d4f58dc86118ebaff890
riifa <leaf.trd@gmail.com>
2025-08-25 16:10:49 +0800

    passive

---
+++ changelog-units.js
@@ -0,0 +1,50 @@
+const newUnitsData = [
+    {
+        name: "Banana",
+        image: "assets/units/placeholder.png",
+        rarity: "Unbound",
+        element: "Green",
+        placement: "Ground",
+        passive: "koishi destroy map bwaaaaa"
+    },
+    {
+        name: "Banana 2",
+        image: "assets/units/placeholder.png",
+        rarity: "Unbound",
+        element: "Green",
+        placement: "Ground",
+        passive: "koishi destroy map bwaaaaa"
+    },
+    {
+        name: "Banana 3",
+        image: "assets/units/placeholder.png",
+        rarity: "Unbound",
+        element: "Green",
+        placement: "Ground",
+        passive: "koishi destroy map bwaaaaa"
+    },
+    {
+        name: "Banana 5",
+        image: "assets/units/placeholder.png",
+        rarity: "Unbound",
+        element: "Green",
+        placement: "Ground",
+        passive: "koishi destroy map bwaaaaa"
+    },
+    {
+        name: "Banana 6",
+        image: "assets/units/placeholder.png",
+        rarity: "Unbound",
+        element: "Green",
+        placement: "Ground",
+        passive: "koishi destroy map bwaaaaa"
+    },
+    {
+        name: "Banana 7",
+        image: "assets/units/placeholder.png",
+        rarity: "Unbound",
+        element: "Green",
+        placement: "Ground",
+        passive: "koishi destroy map bwaaaaa"
+    }
+];
