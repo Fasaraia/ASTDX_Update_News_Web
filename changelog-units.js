2e0c9621e4ce47dbe579d6d6eeb7e62ebf84581b
Fas <Fasaraiaaaa@gmail.com>
2025-08-27 13:29:28 +1200

    maybe added giorno...

--- changelog-units.js
+++ changelog-units.js
@@ -121,11 +121,11 @@ const newUnitsData = [
         placement: "Ground",
         method: "Banner",
         manualAbility: {
-            title: "Manual Ability",
+            title: 'Manual Ability',
             columns: [{
-                heading: "Life Giver",
+                heading: 'Life Giver',
                 content: [
-                    "<span class='cd-desc>(120s CD) - Local",
+                    "<span class='cd-desc>(120s CD) - Local</span>",
                     "Cleanses any Debuffs or Negative Status Effects on units within Star Boy's Range, Including Star Boy himself."
                 ]
             }]
